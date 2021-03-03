710c2b320aeca0f344cbbde3c644c8697a607259
Jacob Andreas <jda@MacBook-Pro-3.local>
2021-03-02 20:25:07 -0500

    new rendering

--- data/shapes/render.py
+++ data/shapes/render.py
@@ -27,7 +27,7 @@ def make_sphere():
     return trimesh.creation.uv_sphere(radius=0.06)
 
 def make_cube():
-    return trimesh.creation.box(extents=(0.13, 0.13, 0.13))
+    return trimesh.creation.box(extents=(0.13, 0.13, 0.13)) 
 
 def make_cylinder():
     return trimesh.creation.cylinder(radius=0.06, height=0.13)
@@ -47,30 +47,33 @@ def sample_object():
     shape = random.choice(list(SHAPES.keys()))
 
     obj = SHAPES[shape]()
-    obj.visual.vertex_colors = COLORS[color]
+    rgb = COLORS[color] + 0.5 * (random.random(size=3) - 0.5)
+    rgb = np.clip(rgb, 0, 1)
+    obj.visual.vertex_colors = rgb
     mesh = pyrender.Mesh.from_trimesh(obj)
-
+    
     return f"{color} {shape}", mesh
 
 def sample_coords(cx=0, cy=0, cz=0):
     return (
-        cx + np.random.random() * 0.3 - 0.15,
-        cy + np.random.random() * 0.3 - 0.15,
-        cz
+        cx + np.random.random() * 0.05,
+        cy + np.random.random() * 0.05,
+        cz + np.random.random() * 0.05,
     )
 
 def sample_scene():
     scene = pyrender.Scene()
     obj1_desc, obj1 = sample_object()
-    #p1x, p1y, p1z = sample_coords()
-    p1x, p1y, p1z = 0, -.15, 0
+    p1x, p1y, p1z = sample_coords(0, -.15, 0)
+    #p1x, p1y, p1z = 0, -.15, 0
     scene.add(obj1, pose=make_pose(p1x, p1y, p1z))
     desc = obj1_desc
-
+    
     if random.random() < 0.5:
     #if True:
         obj2_desc, obj2 = sample_object()
-        p2x, p2y, p2z = 0, .15, 0
+        p2x, p2y, p2z = sample_coords(0, .15, 0)
+        #p2x, p2y, p2z = 0, .15, 0
         #scene.add(obj2, pose=make_pose(*sample_coords(-p1x, -p1y, p1z)))
         scene.add(obj2, pose=make_pose(p2x, p2y, p2z))
         desc += " , " + obj2_desc
@@ -115,8 +118,7 @@ def sample_scene():
 
 def main():
     data = []
-    splits = {"train":[],"test":[]}
-    for i in tqdm(range(2000)):
+    for i in tqdm(range(1000)):
         obj_desc, img = sample_scene()
         img_path = f"images/{i}.png"
         imageio.imsave(img_path, img)
@@ -124,17 +126,8 @@ def main():
             "image": img_path,
             "description": obj_desc
         })
-        if "red cube" in obj_desc:
-            splits["test"].append(i)
-        else:
-            splits["train"].append(i)
-
     with open("data.json", "w") as writer:
         json.dump(data, writer)
-
-    with open("splits.json", "w") as writer:
-        json.dump(splits, writer)
-
 
 if __name__ == "__main__":
     main()
