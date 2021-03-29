d555af6c9fb015ebe5a08b1c8199ef8934a3e5d1
Ekin Akyürek <akyurekekin@gmail.com>
2021-03-29 10:48:43 -0400

    clevr interactive

---
+++ scripts/clevr_vae_interactive.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+n_batch=128
+h_dim=32
+seed=0
+modeltype=VAE
+datatype=clevr
+i=0
+for n_latent in 16 32 64; do
+  for beta in 1.0 5.0 10.0; do
+    for lr in 0.001 0.0003; do
+          i=$((i + 1));
+          if [[ $i -eq $1 ]]; then
+            exp_folder="vis/${datatype}/${modeltype}/beta_${beta}_ncodes_${n_codes}_ldim_${n_latent}_dim_${h_dim}_lr_${lr}/logs/"
+            mkdir -p $exp_folder
+            PYTHONHASHSEED=${seed} python -u main.py \
+                                --seed ${seed} \
+                                --n_batch ${n_batch} \
+                      	        --n_latent ${n_latent} \
+                             	  --h_dim ${h_dim} \
+                                --beta ${beta} \
+                                --modeltype ${modeltype} \
+                                --datatype ${datatype} \
+                      	        --lr ${lr}
+          fi
+    done
+  done
+done
