807aee938cd3f88facbf3ce6bd7797d915f68285
ekinakyurek <akyurekekin@gmail.com>
2021-09-29 10:36:29 -0400

    array jobs

--- scripts/filter_model_interactive.sh
+++ scripts/filter_model_interactive.sh
@@ -1,36 +1,41 @@
 #!/bin/bash
-n_batch=256
-n_latent=64
-h_dim=64
+
+#SBATCH -o filter.out-%A-%a
+#SBATCH --qos=high
+#SBATCH --time=24:00:00
+#SBATCH --gres=gpu:volta:1
+#SBATCH --cpus-per-task=20
+#SBATCH --tasks-per-node=1
+#SBATCH --array=1-45
+
+n_batch=64
 n_epoch=50
-beta=5
 seed=0
 modeltype=FilterModel
 datatype=clevr
 i=0
-for lr in 0.001; do
-  i=$((i + 1));
-  if [[ $i -eq $1 ]]; then
-    exp_folder="vis/${datatype}/${modeltype}/dim_${n_latent}_lr_${lr}/logs/"
-    mkdir -p $exp_folder
-    PYTHONHASHSEED=${seed} python -u main.py \
-                        --seed ${seed} \
-                        --n_batch ${n_batch} \
-              	        --n_latent ${n_latent} \
-                        --h_dim ${h_dim} \
-                        --n_epoch ${n_epoch} \
-                        --modeltype ${modeltype} \
-                        --datatype ${datatype} \
-                        --filter_model \
-                        --lr ${lr} \
-                        --n_workers 8 \
-                        --beta ${beta} \
-                        --tensorboard 'tb_logs'\
-                        # --gpu 0 \
-                        # --distributed \
-                        # --multiprocessing_distributed \
-                        # --rank 0 \
-                        # --world_size 1 \
-
-  fi
+for beta in  0.1 0.5 1.0 2.0 5.0; do
+    for n_latent in 16 32 48; do
+	     for lr in 0.00075 0.0005 0.00025; do
+	         h_dim=$n_latent
+		 i=$((i + 1));
+	         if [[ $i -eq $SLURM_ARRAY_TASK_ID ]]; then
+          	     exp_folder="vis/${datatype}/${modeltype}/dim_${n_latent}_lr_${lr}_beta_${beta}"
+          	     mkdir -p $exp_folder
+          	     PYTHONHASHSEED=${seed} python -u main.py \
+                                   --seed ${seed} \
+                                   --n_batch ${n_batch} \
+                        	   --n_latent ${n_latent} \
+                                   --h_dim ${h_dim} \
+                                   --n_epoch ${n_epoch} \
+                                   --modeltype ${modeltype} \
+                                   --datatype ${datatype} \
+                                   --filter_model \
+                                   --lr ${lr} \
+                                   --n_workers 8 \
+                                   --beta ${beta} \
+                                   --tensorboard  'tb_logs' 	      
+		 fi
+       done
+    done
 done
