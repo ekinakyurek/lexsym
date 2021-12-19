060c9e6dab7312e3bca9b5ca0b2ce77186ed7fd1
ekinakyurek <akyurekekin@gmail.com>
2021-12-19 17:04:26 -0500

    improve evaluate

--- evaluate.py
+++ evaluate.py
@@ -39,34 +39,47 @@ def single_file_eval(prediction_file, root, split):
             hashmap[question["image"]] = [i]
             
     category_map = {}
+    data = []
     with open(prediction_file) as f:
         for line in f:
-            question, answer, pred, image = line.strip().split('\t')
-            question = question.replace('<unk>', '')
-            ids = hashmap[image]
-            pquestions = [questions[i]['question_processed'] for i in ids]
-            try:
-                i = pquestions.index(question)
-                question_info = questions[ids[i]]
-            except:
-                print(question)
-                print(pquestions)
-                pdb.set_trace()
-            family_index = question_info['program'][-1].get('function')
-            if family_index not in category_map:
-                category_map[family_index] = [answer==pred]
-                category_map['all'] = [answer==pred]
-            else:
-                category_map[family_index].append(answer==pred)
-                category_map['all'].append(answer==pred)
-    
+            data.append(line)
+
+    for line in data:
+        question, answer, pred, image = line.strip().split('\t')
+        question = question.replace('<unk>', '')
+        ids = hashmap[image]
+        pquestions = [questions[i]['question_processed'] for i in ids]
+        try:
+            i = pquestions.index(question)
+            question_info = questions[ids[i]]
+        except:
+            print(question)
+            print(pquestions)
+            pdb.set_trace()
+        family_index = question_info['program'][-1].get('function')
+        if family_index not in category_map:
+            category_map[family_index] = [answer==pred]
+            category_map['all'] = [answer==pred]
+        else:
+            category_map[family_index].append(answer==pred)
+            category_map['all'].append(answer==pred)
+
+    reduced_categories = {'Count': ['count'],
+                          'Exist': ['exist'], 
+                          'Compare Numbers': ['equal_integer'],
+                          "Query Attribute": ['query_color', 'query_shape', 'query_size', 'query_material'], 
+                          "Compare Attribute": ['greater_than', 'less_than', 'equal_shape', 'equal_material', 'equal_size']} 
+
+    for k, v in reduced_categories.items():
+        reduced_categories[k] = np.concatenate([category_map[vi] for vi in v], axis=0)
+    reduced_categories['all'] = np.concatenate([category_map[vi] for vi in category_map.keys()], axis=0)
     for k, v in category_map.items():
         category_map[k] = np.mean(v)
-    
+        
     with open(prediction_file.replace('predictions', 'metrics').replace('.txt', '.json'), "w") as f:
         json.dump(category_map, f)
         
-    return category_map
+    return reduced_categories
           
 
 def _single_eval(prediction_file, dataroot):
@@ -88,11 +101,14 @@ def exp_eval(prediction_files, dataroot):
         eval_seed = _single_eval(file, dataroot)
         evals.append(eval_seed)
     
-    avg_eval = {}
+    eval_labels = {}
+    metrics = {'mean': {}, 'std': {}}
     for k in evals[0].keys():
-        avg_eval[k] = np.mean([eval[k] for eval in evals]) 
+        eval_labels[k] = np.concatenate([eval[k] for eval in evals]) 
+        metrics['mean'][k] = np.mean(eval_labels[k])
+        metrics['std'][k] = np.std(eval_labels[k])
     
-    return avg_eval
+    return metrics, eval_labels
 
 from prettytable import PrettyTable
 
@@ -101,21 +117,24 @@ def main(_):
     results = {}
     x = PrettyTable()
     field_names_set = False
-    for dataroot in ("original_clevr",):
+    for dataroot in ("clevr", "original_clevr"):
         if dataroot not in results:
             results[dataroot] = {}
-        for k in ("_no_swap_", "_"):
+        for k in ("_", "_substitute_", "_no_swap_"):
+            if dataroot == "original_clevr" and k == "_substitute_":
+                continue
             prediction_files = []
             for seed in range(0, 4):
-                prediction_template = f"clip_exp{k}vqa_seed_{seed}_{dataroot}/clevr/VQA/beta_1.0_ldim_64_dim_128_lr_1.0/predictions_test_792000.txt"
+                prediction_template = f"clip_exp{k}vqa_seed_{seed}_{dataroot}/clevr/VQA/beta_1.0_ldim_64_dim_128_lr_1.0/predictions_test_800000.txt"
                 prediction_files.append(prediction_template)
                 
-            results[dataroot][k] = exp_eval(prediction_files, "data/"+dataroot)
+            exp_metrics, exp_labels = exp_eval(prediction_files, "data/"+dataroot)
+            results[dataroot][k] = exp_metrics['mean']
             
             if not field_names_set:
                 x.field_names = ['data', 'model'] + list(results[dataroot][k].keys())
                 field_names_set = True
-            x.add_row([dataroot, k] + [str(round(results[dataroot][k][i],3)) for i in x.field_names[2:]])
+            x.add_row([dataroot, k] + [str(round(results[dataroot][k][i], 3)) for i in x.field_names[2:]])
             print(x)
             
     print(x)
